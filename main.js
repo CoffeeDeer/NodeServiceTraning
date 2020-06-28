a5ce20cfbd2e249f52731041f09166e6e4eaf210
김현기 <11609005+rubi1541@users.noreply.github.com>
2020-06-28 22:13:48 +0900

    express default page

---
+++ main.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var path = require('path');
+//var routes = require('./routes/index');
+
+var app = express();
+
+app.use(express.static('static'));
+
+// view engine setup
+app.set('views', path.join(__dirname, 'static'));
+app.set('view engine', 'pug');
+
+app.get('/', function (req, res) {
+    res.render('test',{
+        title: 'Hey',
+        message: 'Hello there!'
+    });
+});
+
+app.listen(80, () => {
+    console.log('Express App on port 80!');
+});
+
+
+//app.use('/', routes);
